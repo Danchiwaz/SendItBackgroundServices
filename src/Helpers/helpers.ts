cb7c029dffd4131b1ce5ea6beab38d606443e5af
Danchiwaz <mainadanielchiwaz@gmail.com>
2022-09-15 15:02:13 +0300

    Added register email on the service

---
+++ src/Helpers/helpers.ts
@@ -0,0 +1,5 @@
+export const parseDatabaseData = (data:any, name:string) =>{
+    const datareturned = data.rows[0][name];
+    return datareturned;
+    // console.log(users.rows[0]["getalluserstosendemail"]);
+}
