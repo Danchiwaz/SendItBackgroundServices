657a5a4eecae9a9ad422b08afa45af105e577e39
Danchiwaz <mainadanielchiwaz@gmail.com>
2022-09-19 11:15:41 +0300

    Remove ignored files

--- dist/Helpers/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseDatabaseData = void 0;
-const parseDatabaseData = (data, name) => {
-    const datareturned = data.rows[0][name];
-    return datareturned !== null && datareturned !== void 0 ? datareturned : [];
-    // console.log(users.rows[0]["getalluserstosendemail"]);
-};
-exports.parseDatabaseData = parseDatabaseData;
--- dist/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseDatabaseData = void 0;
-const parseDatabaseData = (data, name) => {
-    const datareturned = data.rows[0][name];
-    return datareturned;
-    // console.log(users.rows[0]["getalluserstosendemail"]);
-};
-exports.parseDatabaseData = parseDatabaseData;
